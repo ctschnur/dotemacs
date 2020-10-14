5dd5fd55eee9ffcd5a25bfb992705828916d441e
ctschnur <christopher.schnur@neel.cnrs.fr>
2020-10-14 16:07:38 +0200

    cleaning up

--- .emacs.d/elisp/in-dev/cs-magit.el
+++ .emacs.d/elisp/in-dev/cs-magit.el
@@ -25,6 +25,37 @@
 ;;; Code:
 
 
+;; from https://gist.github.com/ieure/883725
+(eval-after-load "magit"
+  '(progn
+     (defun magit-browse-github (commit)
+       "Open a browser pointing to the tracked branch on GitHub.
+        With prefix argument, go to the commit HEAD is at."
+       (interactive "p")
+       (let* ((branch (magit-get-current-branch))
+              (remote (and branch (magit-get "branch" branch "remote")))
+              (remote-branch (or (and branch (magit-remote-branch-for branch))
+                                 branch))
+              (remote-url (magit-get "remote" remote "url"))
+              (head (magit-git-string "log" "--max-count=1"
+                                      "--pretty=format:%h"))
+              (remote-tree (if (> commit 1) head remote-branch)))
+
+         (browse-url
+          (cond ((string-match "git@github\\.com" remote-url)
+                 (replace-regexp-in-string
+                  "^git@github.com:\\(.*?\\)\\(\\.git\\)?$"
+                  (format "https://github.com/\\1/tree/%s" remote-tree)
+                  remote-url))
+                ((string-match "https?://github\\.com" remote-url)
+                 (replace-regexp-in-string
+                  "^\\(https?://github\\.com/.*?\\)\\(\\.git\\)?$"
+                  (format "\\1/tree/%s" remote-tree) remote-url))
+                (t (error "Tracked branch is not on GitHub."))))))
+
+     (define-key magit-mode-map "\C-ch" 'magit-browse-github)))
+
+
 
 (provide 'cs-magit)
 ;;; cs-magit.el ends here
